b78aabc6a733f56b70729e2df9db8e128156253d
hellen lopez <hellenl2012@gmail.com>
2016-10-03 15:42:05 -0600

    commite#1

---
+++ Torres de hanoi/JCVDTorresdeHanoi/Form1.cs
@@ -0,0 +1,52 @@
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+
+namespace TorresdeHanoi
+{
+    public partial class torresHanoi : Form
+    {
+       
+
+        public torresHanoi()
+        {
+            InitializeComponent();
+            
+        }
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+        private void panel1_MouseMove(object sender, MouseEventArgs e) { }
+        private void panel1_MouseUp(object sender, MouseEventArgs e) { }
+        public void dibuja() { }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+        private void timer1_Tick(object sender, EventArgs e) { }
+        private void button5_Click_1(object sender, EventArgs e) { }
+        private void btn_demo_Click(object sender, EventArgs e) { }
+        private void btn_jugar_Click(object sender, EventArgs e) { }
+        
+        private void torresHanoi_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
